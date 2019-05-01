938fd722664a60a7599525ad3305b46b8ef5d7d7
nikolay miz <leon-msn@hotmail.de>
2019-05-01 15:39:59 +0200

    test commit 2

--- cg3_u3/cmake-build-debug/cmake_install.cmake
+++ cg3_u3/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/don/Desktop/cg19/cg3_u3
+# Install script for directory: /home/leoon/Schreibtisch/neues sem/CG1/übung/Donn_Git/cg19/cg3_u3
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/don/Desktop/cg19/cg3_u3/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/leoon/Schreibtisch/neues sem/CG1/übung/Donn_Git/cg19/cg3_u3/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
