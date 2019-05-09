212ce4914f57edcbbf904ee188d9c3a4edf13dd4
bdonr <joker_tn@web.de>
2019-05-09 15:48:28 -0700

    sonne bewegt sich + flugzeugsteuerung

---
+++ cg3_u3/cmake-build-debug/CMakeFiles/cg3_u3.dir/DependInfo.cmake
@@ -0,0 +1,100 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/don/Desktop/cg19/cg3_u3/scg3/extern/glew/src/glew.c" "/home/don/Desktop/cg19/cg3_u3/cmake-build-debug/CMakeFiles/cg3_u3.dir/scg3/extern/glew/src/glew.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../scg3"
+  "../scg3/extern/glew/include"
+  "../scg3/extern/glew/include/GL"
+  "../scg3/extern/glm/glm"
+  "../scg3/extern/glm/glm/detail"
+  "../scg3/extern/glm/glm/ext"
+  "../scg3/extern/glm/glm/gtc"
+  "../scg3/extern/glm/glm/gtx"
+  "../scg3/extern/glm/glm/simd"
+  "../scg3/extern/stb_image"
+  "../scg3/src"
+  "../scg3/src_ext"
+  )
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/don/Desktop/cg19/cg3_u3/scg3/extern/glm/glm/detail/glm.cpp" "/home/don/Desktop/cg19/cg3_u3/cmake-build-debug/CMakeFiles/cg3_u3.dir/scg3/extern/glm/glm/detail/glm.cpp.o"
+  "/home/don/Desktop/cg19/cg3_u3/scg3/src/Animation.cpp" "/home/don/Desktop/cg19/cg3_u3/cmake-build-debug/CMakeFiles/cg3_u3.dir/scg3/src/Animation.cpp.o"
+  "/home/don/Desktop/cg19/cg3_u3/scg3/src/BumpMapCore.cpp" "/home/don/Desktop/cg19/cg3_u3/cmake-build-debug/CMakeFiles/cg3_u3.dir/scg3/src/BumpMapCore.cpp.o"
+  "/home/don/Desktop/cg19/cg3_u3/scg3/src/Camera.cpp" "/home/don/Desktop/cg19/cg3_u3/cmake-build-debug/CMakeFiles/cg3_u3.dir/scg3/src/Camera.cpp.o"
+  "/home/don/Desktop/cg19/cg3_u3/scg3/src/CameraController.cpp" "/home/don/Desktop/cg19/cg3_u3/cmake-build-debug/CMakeFiles/cg3_u3.dir/scg3/src/CameraController.cpp.o"
+  "/home/don/Desktop/cg19/cg3_u3/scg3/src/ColorCore.cpp" "/home/don/Desktop/cg19/cg3_u3/cmake-build-debug/CMakeFiles/cg3_u3.dir/scg3/src/ColorCore.cpp.o"
+  "/home/don/Desktop/cg19/cg3_u3/scg3/src/Composite.cpp" "/home/don/Desktop/cg19/cg3_u3/cmake-build-debug/CMakeFiles/cg3_u3.dir/scg3/src/Composite.cpp.o"
+  "/home/don/Desktop/cg19/cg3_u3/scg3/src/Controller.cpp" "/home/don/Desktop/cg19/cg3_u3/cmake-build-debug/CMakeFiles/cg3_u3.dir/scg3/src/Controller.cpp.o"
+  "/home/don/Desktop/cg19/cg3_u3/scg3/src/Core.cpp" "/home/don/Desktop/cg19/cg3_u3/cmake-build-debug/CMakeFiles/cg3_u3.dir/scg3/src/Core.cpp.o"
+  "/home/don/Desktop/cg19/cg3_u3/scg3/src/CubeMapCore.cpp" "/home/don/Desktop/cg19/cg3_u3/cmake-build-debug/CMakeFiles/cg3_u3.dir/scg3/src/CubeMapCore.cpp.o"
+  "/home/don/Desktop/cg19/cg3_u3/scg3/src/GeometryCore.cpp" "/home/don/Desktop/cg19/cg3_u3/cmake-build-debug/CMakeFiles/cg3_u3.dir/scg3/src/GeometryCore.cpp.o"
+  "/home/don/Desktop/cg19/cg3_u3/scg3/src/GeometryCoreFactory.cpp" "/home/don/Desktop/cg19/cg3_u3/cmake-build-debug/CMakeFiles/cg3_u3.dir/scg3/src/GeometryCoreFactory.cpp.o"
+  "/home/don/Desktop/cg19/cg3_u3/scg3/src/Group.cpp" "/home/don/Desktop/cg19/cg3_u3/cmake-build-debug/CMakeFiles/cg3_u3.dir/scg3/src/Group.cpp.o"
+  "/home/don/Desktop/cg19/cg3_u3/scg3/src/InfoTraverser.cpp" "/home/don/Desktop/cg19/cg3_u3/cmake-build-debug/CMakeFiles/cg3_u3.dir/scg3/src/InfoTraverser.cpp.o"
+  "/home/don/Desktop/cg19/cg3_u3/scg3/src/KeyboardController.cpp" "/home/don/Desktop/cg19/cg3_u3/cmake-build-debug/CMakeFiles/cg3_u3.dir/scg3/src/KeyboardController.cpp.o"
+  "/home/don/Desktop/cg19/cg3_u3/scg3/src/Leaf.cpp" "/home/don/Desktop/cg19/cg3_u3/cmake-build-debug/CMakeFiles/cg3_u3.dir/scg3/src/Leaf.cpp.o"
+  "/home/don/Desktop/cg19/cg3_u3/scg3/src/Light.cpp" "/home/don/Desktop/cg19/cg3_u3/cmake-build-debug/CMakeFiles/cg3_u3.dir/scg3/src/Light.cpp.o"
+  "/home/don/Desktop/cg19/cg3_u3/scg3/src/LightPosition.cpp" "/home/don/Desktop/cg19/cg3_u3/cmake-build-debug/CMakeFiles/cg3_u3.dir/scg3/src/LightPosition.cpp.o"
+  "/home/don/Desktop/cg19/cg3_u3/scg3/src/MaterialCore.cpp" "/home/don/Desktop/cg19/cg3_u3/cmake-build-debug/CMakeFiles/cg3_u3.dir/scg3/src/MaterialCore.cpp.o"
+  "/home/don/Desktop/cg19/cg3_u3/scg3/src/MouseController.cpp" "/home/don/Desktop/cg19/cg3_u3/cmake-build-debug/CMakeFiles/cg3_u3.dir/scg3/src/MouseController.cpp.o"
+  "/home/don/Desktop/cg19/cg3_u3/scg3/src/Node.cpp" "/home/don/Desktop/cg19/cg3_u3/cmake-build-debug/CMakeFiles/cg3_u3.dir/scg3/src/Node.cpp.o"
+  "/home/don/Desktop/cg19/cg3_u3/scg3/src/OrthographicCamera.cpp" "/home/don/Desktop/cg19/cg3_u3/cmake-build-debug/CMakeFiles/cg3_u3.dir/scg3/src/OrthographicCamera.cpp.o"
+  "/home/don/Desktop/cg19/cg3_u3/scg3/src/PerspectiveCamera.cpp" "/home/don/Desktop/cg19/cg3_u3/cmake-build-debug/CMakeFiles/cg3_u3.dir/scg3/src/PerspectiveCamera.cpp.o"
+  "/home/don/Desktop/cg19/cg3_u3/scg3/src/PreTraverser.cpp" "/home/don/Desktop/cg19/cg3_u3/cmake-build-debug/CMakeFiles/cg3_u3.dir/scg3/src/PreTraverser.cpp.o"
+  "/home/don/Desktop/cg19/cg3_u3/scg3/src/RenderState.cpp" "/home/don/Desktop/cg19/cg3_u3/cmake-build-debug/CMakeFiles/cg3_u3.dir/scg3/src/RenderState.cpp.o"
+  "/home/don/Desktop/cg19/cg3_u3/scg3/src/RenderTraverser.cpp" "/home/don/Desktop/cg19/cg3_u3/cmake-build-debug/CMakeFiles/cg3_u3.dir/scg3/src/RenderTraverser.cpp.o"
+  "/home/don/Desktop/cg19/cg3_u3/scg3/src/Renderer.cpp" "/home/don/Desktop/cg19/cg3_u3/cmake-build-debug/CMakeFiles/cg3_u3.dir/scg3/src/Renderer.cpp.o"
+  "/home/don/Desktop/cg19/cg3_u3/scg3/src/ShaderCore.cpp" "/home/don/Desktop/cg19/cg3_u3/cmake-build-debug/CMakeFiles/cg3_u3.dir/scg3/src/ShaderCore.cpp.o"
+  "/home/don/Desktop/cg19/cg3_u3/scg3/src/ShaderCoreFactory.cpp" "/home/don/Desktop/cg19/cg3_u3/cmake-build-debug/CMakeFiles/cg3_u3.dir/scg3/src/ShaderCoreFactory.cpp.o"
+  "/home/don/Desktop/cg19/cg3_u3/scg3/src/Shape.cpp" "/home/don/Desktop/cg19/cg3_u3/cmake-build-debug/CMakeFiles/cg3_u3.dir/scg3/src/Shape.cpp.o"
+  "/home/don/Desktop/cg19/cg3_u3/scg3/src/StandardRenderer.cpp" "/home/don/Desktop/cg19/cg3_u3/cmake-build-debug/CMakeFiles/cg3_u3.dir/scg3/src/StandardRenderer.cpp.o"
+  "/home/don/Desktop/cg19/cg3_u3/scg3/src/Texture2DCore.cpp" "/home/don/Desktop/cg19/cg3_u3/cmake-build-debug/CMakeFiles/cg3_u3.dir/scg3/src/Texture2DCore.cpp.o"
+  "/home/don/Desktop/cg19/cg3_u3/scg3/src/TextureCore.cpp" "/home/don/Desktop/cg19/cg3_u3/cmake-build-debug/CMakeFiles/cg3_u3.dir/scg3/src/TextureCore.cpp.o"
+  "/home/don/Desktop/cg19/cg3_u3/scg3/src/TextureCoreFactory.cpp" "/home/don/Desktop/cg19/cg3_u3/cmake-build-debug/CMakeFiles/cg3_u3.dir/scg3/src/TextureCoreFactory.cpp.o"
+  "/home/don/Desktop/cg19/cg3_u3/scg3/src/TransformAnimation.cpp" "/home/don/Desktop/cg19/cg3_u3/cmake-build-debug/CMakeFiles/cg3_u3.dir/scg3/src/TransformAnimation.cpp.o"
+  "/home/don/Desktop/cg19/cg3_u3/scg3/src/Transformation.cpp" "/home/don/Desktop/cg19/cg3_u3/cmake-build-debug/CMakeFiles/cg3_u3.dir/scg3/src/Transformation.cpp.o"
+  "/home/don/Desktop/cg19/cg3_u3/scg3/src/Traverser.cpp" "/home/don/Desktop/cg19/cg3_u3/cmake-build-debug/CMakeFiles/cg3_u3.dir/scg3/src/Traverser.cpp.o"
+  "/home/don/Desktop/cg19/cg3_u3/scg3/src/ViewState.cpp" "/home/don/Desktop/cg19/cg3_u3/cmake-build-debug/CMakeFiles/cg3_u3.dir/scg3/src/ViewState.cpp.o"
+  "/home/don/Desktop/cg19/cg3_u3/scg3/src/Viewer.cpp" "/home/don/Desktop/cg19/cg3_u3/cmake-build-debug/CMakeFiles/cg3_u3.dir/scg3/src/Viewer.cpp.o"
+  "/home/don/Desktop/cg19/cg3_u3/scg3/src/scg_internals.cpp" "/home/don/Desktop/cg19/cg3_u3/cmake-build-debug/CMakeFiles/cg3_u3.dir/scg3/src/scg_internals.cpp.o"
+  "/home/don/Desktop/cg19/cg3_u3/scg3/src/scg_stb_image.cpp" "/home/don/Desktop/cg19/cg3_u3/cmake-build-debug/CMakeFiles/cg3_u3.dir/scg3/src/scg_stb_image.cpp.o"
+  "/home/don/Desktop/cg19/cg3_u3/scg3/src/scg_utilities.cpp" "/home/don/Desktop/cg19/cg3_u3/cmake-build-debug/CMakeFiles/cg3_u3.dir/scg3/src/scg_utilities.cpp.o"
+  "/home/don/Desktop/cg19/cg3_u3/scg3/src_ext/StereoCamera.cpp" "/home/don/Desktop/cg19/cg3_u3/cmake-build-debug/CMakeFiles/cg3_u3.dir/scg3/src_ext/StereoCamera.cpp.o"
+  "/home/don/Desktop/cg19/cg3_u3/scg3/src_ext/StereoRenderer.cpp" "/home/don/Desktop/cg19/cg3_u3/cmake-build-debug/CMakeFiles/cg3_u3.dir/scg3/src_ext/StereoRenderer.cpp.o"
+  "/home/don/Desktop/cg19/cg3_u3/scg3/src_ext/StereoRendererActive.cpp" "/home/don/Desktop/cg19/cg3_u3/cmake-build-debug/CMakeFiles/cg3_u3.dir/scg3/src_ext/StereoRendererActive.cpp.o"
+  "/home/don/Desktop/cg19/cg3_u3/scg3/src_ext/StereoRendererAnaglyph.cpp" "/home/don/Desktop/cg19/cg3_u3/cmake-build-debug/CMakeFiles/cg3_u3.dir/scg3/src_ext/StereoRendererAnaglyph.cpp.o"
+  "/home/don/Desktop/cg19/cg3_u3/scg3/src_ext/StereoRendererPassive.cpp" "/home/don/Desktop/cg19/cg3_u3/cmake-build-debug/CMakeFiles/cg3_u3.dir/scg3/src_ext/StereoRendererPassive.cpp.o"
+  "/home/don/Desktop/cg19/cg3_u3/scg3_example/main.cpp" "/home/don/Desktop/cg19/cg3_u3/cmake-build-debug/CMakeFiles/cg3_u3.dir/scg3_example/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../scg3"
+  "../scg3/extern/glew/include"
+  "../scg3/extern/glew/include/GL"
+  "../scg3/extern/glm/glm"
+  "../scg3/extern/glm/glm/detail"
+  "../scg3/extern/glm/glm/ext"
+  "../scg3/extern/glm/glm/gtc"
+  "../scg3/extern/glm/glm/gtx"
+  "../scg3/extern/glm/glm/simd"
+  "../scg3/extern/stb_image"
+  "../scg3/src"
+  "../scg3/src_ext"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
