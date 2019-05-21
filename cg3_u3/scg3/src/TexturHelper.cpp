0f01f47b8b3c78831a143a5bd684ddb8843cbe32
bdonr <joker_tn@web.de>
2019-05-21 10:10:08 -0700

    refactoring part 3

---
+++ cg3_u3/scg3/src/TexturHelper.cpp
@@ -0,0 +1,5 @@
+//
+// Created by don on 21.05.19.
+//
+
+#include "TexturHelper.h"
---
+++ cg3_u3/scg3/src/TexturHelper.h
@@ -0,0 +1,14 @@
+//
+// Created by don on 21.05.19.
+//
+
+#ifndef CG3_U3_TEXTURHELPER_H
+#define CG3_U3_TEXTURHELPER_H
+
+
+class TexturHelper {
+
+};
+
+
+#endif //CG3_U3_TEXTURHELPER_H
