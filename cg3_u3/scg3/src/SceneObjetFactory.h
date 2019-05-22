667b16469020ca26356723d8da49407ad32e0a30
bdonr <joker_tn@web.de>
2019-05-21 20:39:21 -0700

    refactor part 4

---
+++ cg3_u3/scg3/src/SceneObjetFactory.h
@@ -0,0 +1,14 @@
+//
+// Created by don on 21.05.19.
+//
+
+#ifndef CG3_U3_SCENEOBJETFACTORY_H
+#define CG3_U3_SCENEOBJETFACTORY_H
+
+
+class SceneObjetFactory {
+
+};
+
+
+#endif //CG3_U3_SCENEOBJETFACTORY_H
